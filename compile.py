6cc902c4fdb50a52eba7115596c3ed2b393a4e2d
Jakub Hanko <60473007+JakubHanko@users.noreply.github.com>
2023-10-13 20:41:24 +0200

    Add compilation of the website

--- compile.py
+++ compile.py
@@ -3,6 +3,7 @@ import marko
 from marko.block import Heading, List
 from jinja2 import Environment, PackageLoader, select_autoescape
 from typing import List
+import os
 
 from dataclasses import dataclass
 
@@ -26,11 +27,9 @@ def get_list_from_section(document, section):
     section_found = False
     for node in document.children:
         if type(node) is Heading:
-            print(node.children[0].children)
             section_found = node.children[0].children == section
 
         if type(node) is marko.block.List and section_found:
-            print([item.children[0].children[0].children for item in node.children])
             return [item.children[0].children[0].children for item in node.children]
 
 
@@ -41,17 +40,24 @@ def get_recipe_instructions(document):
 
 def get_recipe_notes(document):
     return get_list_from_section(document, NOTES_STR)
-    
 
-def parse_recipe_file(recipe_file):
-    with open(recipe_file) as f:
+
+def get_recipe_slug(recipe_filename):
+    recipe_filename = recipe_filename.split(os.sep)[-1]
+    recipe_filename = recipe_filename.split('.')[0]
+
+    return recipe_filename.replace('_', '-')    
+
+def parse_recipe_file(recipe_filename):
+    with open(recipe_filename) as f:
         recipe_doc = marko.parse(f.read())
 
     return Recipe(
         name=get_recipe_name(recipe_doc),
         ingredients=get_recipe_ingredients(recipe_doc),
         instructions=get_recipe_instructions(recipe_doc),
-        notes=get_recipe_notes(recipe_doc)
+        notes=get_recipe_notes(recipe_doc),
+        slug=get_recipe_slug(recipe_filename)
     )
 
     
@@ -62,7 +68,7 @@ class Recipe:
     ingredients: List[str]
     instructions: List[str]
     notes: List[str]
-    
+    slug: str
 
 if __name__ == "__main__":
     recipe_filenames = glob.glob('recipes/*.md')
@@ -76,5 +82,8 @@ if __name__ == "__main__":
 
     template = env.get_template("recipe.html")
 
-    recipe = recipes[2]
-    template.stream(recipe_name=recipe.name, ingredients=recipe.ingredients, instructions=recipe.instructions, notes=recipe.notes).dump('test.html')
+    for recipe in recipes:
+        template.stream(recipe_name=recipe.name, ingredients=recipe.ingredients, instructions=recipe.instructions, notes=recipe.notes).dump(f"docs/{recipe.slug}.html")
+
+    index_template = env.get_template("index.html")
+    index_template.stream(recipes=recipes).dump('docs/index.html')
