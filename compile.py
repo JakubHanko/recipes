2b9005f6dee273540534f26f1c7cc534fbd4462a
Jakub Hanko <60473007+JakubHanko@users.noreply.github.com>
2023-10-16 10:46:43 +0200

    Add support for configuration file

--- compile.py
+++ compile.py
@@ -1,7 +1,9 @@
 import argparse
 import glob
+import json
 import os
 import shutil
+import sys
 
 from jinja2 import Environment, PackageLoader, select_autoescape
 
@@ -14,6 +16,14 @@ if __name__ == "__main__":
     parser.add_argument("--deploy", required=False, action="store_true")
     deploy_on = parser.parse_args().deploy
 
+    with open("config.json") as f:
+        config = json.load(f)
+
+    if not config.get("output_dir"):
+        sys.exit("Missing 'config.json' file.")
+
+    output_dir = config["output_dir"]
+
     recipe_filenames = glob.glob("recipes/*.md")
 
     recipes = [parse_recipe_file(c) for c in recipe_filenames]
@@ -24,10 +34,10 @@ if __name__ == "__main__":
     env.filters["recipe_category_format"] = recipe_category_format
     env.globals["path_base"] = "" if deploy_on else "../templates/"
 
-    if not os.path.exists("docs/"):
-        os.makedirs("docs")
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    write_recipes(env, recipes)
+    write_recipes(env, recipes, output_dir)
 
     recipes_dict: dict[RecipeCategory, list[Recipe]] = {}
 
@@ -37,6 +47,6 @@ if __name__ == "__main__":
 
         recipes_dict[recipe.category].append(recipe)
 
-    write_index(env, recipes_dict)
+    write_index(env, recipes_dict, output_dir)
 
     shutil.copyfile("templates/styles.css", "docs/styles.css")
--- recipe_writer.py
+++ recipe_writer.py
@@ -1,9 +1,13 @@
+import os
+
 from jinja2 import Environment
 
 from recipe_model import Recipe, RecipeCategory
 
 
-def write_recipes(environment: Environment, recipes: list[Recipe]) -> None:
+def write_recipes(
+    environment: Environment, recipes: list[Recipe], output_dir: str
+) -> None:
     template = environment.get_template("recipe.html")
 
     for recipe in recipes:
@@ -13,12 +17,14 @@ def write_recipes(environment: Environment, recipes: list[Recipe]) -> None:
             instructions=recipe.instructions,
             notes=recipe.notes,
             image=recipe.img_path,
-        ).dump(f"docs/{recipe.slug}.html")
+        ).dump(os.path.join(output_dir, f"{recipe.slug}.html"))
 
 
 def write_index(
-    environment: Environment, recipes: dict[RecipeCategory, list[Recipe]]
+    environment: Environment,
+    recipes: dict[RecipeCategory, list[Recipe]],
+    output_dir: str,
 ) -> None:
     template = environment.get_template("index.html")
 
-    template.stream(recipes=recipes).dump("docs/index.html")
+    template.stream(recipes=recipes).dump(os.path.join(output_dir, "index.html"))
